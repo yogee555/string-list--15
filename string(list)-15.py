a70932cbc873cab60ae360a9d28d408f2f5e387b
YOGEESH K P <148324344+yogee555@users.noreply.github.com>
2024-04-05 11:34:33 +0530

    Add files via upload

---
+++ string(list)-15.py
@@ -0,0 +1,4 @@
+# Sorting a list
+L1 = ['Mango','Cherey','Banana','Guava']  # storing value in L1
+L1.sort()                                 # sort for using formula
+print(L1)                                 # print sort for L1
